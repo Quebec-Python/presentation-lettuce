dc3b6d00693b1f2b3dd125b67dce76a5d60ce16c
Gregory Eric Sanderson <gzou2000@gmail.com>
2013-11-19 21:04:32 -0500

    implement steps

--- tests/factorial/steps.py
+++ tests/factorial/steps.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
-from lettuce import step
+from lettuce import step, world
+
 
 @step(u'Given I have the number "([^"]*)"')
-def given_i_have_the_number_group1(step, group1):
-    assert False, 'This step must be implemented'
+def given_i_have_the_number_group1(step, number):
+    world.number = int(number)
 
 
 @step(u'When I compute its factorial')
 def when_i_compute_its_factorial(step):
-    assert False, 'This step must be implemented'
+    world.factorial = compute_factorial(world.number)
 
 
 @step(u'Then I see the number "([^"]*)"')
-def then_i_see_the_number_group1(step, group1):
-    assert False, 'This step must be implemented'
+def then_i_see_the_number_group1(step, number):
+    number = int(number)
+    error_msg = "Got %d, expected %d" % (world.factorial, number)
+    assert world.factorial == number, error_msg
